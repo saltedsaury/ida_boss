402939f3c5f2cb523a8e8d8ecbce8e85ddf24e9c
zhoushu <zhoushu@primeledger.cn>
2019-03-04 16:57:17 +0800

    init

---
+++ guns-core/src/main/java/com/stylefeng/guns/core/mutidatasource/config/FinancingDataSourceProperties.java
@@ -0,0 +1,77 @@
+package com.stylefeng.guns.core.mutidatasource.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "guns.financing-datasource")
+public class FinancingDataSourceProperties {
+    private String defaultDataSourceName = "dataSourceGuns";
+
+    private String url = "jdbc:mysql://127.0.0.1:3306/eth_account_1_7?autoReconnect=true&useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull";
+
+    private String username = "sue";
+
+    private String password = "110119";
+
+    private String driverClassName = "com.mysql.jdbc.Driver";
+
+    private String validationQuery = "SELECT 'x'";
+
+    public void config(DruidDataSource dataSource) {
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setValidationQuery(validationQuery);
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDefaultDataSourceName() {
+        return defaultDataSourceName;
+    }
+
+    public void setDefaultDataSourceName(String defaultDataSourceName) {
+        this.defaultDataSourceName = defaultDataSourceName;
+    }
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public String getValidationQuery() {
+        return validationQuery;
+    }
+
+    public void setValidationQuery(String validationQuery) {
+        this.validationQuery = validationQuery;
+    }
+}
